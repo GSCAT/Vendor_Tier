4d1ebb139a9039b8e9cbfc7f4fb8a70cf0c4953e
Kevin Bonds <Kevin_Bonds@gap.com>
2016-10-14 13:17:22 -0700

    Join old Tiering/Vendors with new Vendors for new Tiering

--- Change_delim.R
+++ Change_delim.R
@@ -18,5 +18,20 @@ write_delim(Delim_Table, paste(choose_file_directory(), 'Vendor_Tier_table 20160
 
 Vendor_ELC_table <- read_delim(paste(choose_file_directory(), 'Master_Vendor_with_ELC.txt', sep = '/'), delim = '^')
 
+# Tidying data
 spread_Vendor_ELC_table <- Vendor_ELC_table %>%
+                            mutate(Total_FCST_ELC = as.character(Vendor_ELC_table$Total_FCST_ELC))%>%
                               spread(Category,  value= Total_FCST_ELC)
+
+nm_vec <- names(spread_Vendor_ELC_table)[1:5]
+spread_Vendor_ELC_table <- spread_Vendor_ELC_table[c(nm_vec,'Wovens', 'Sweaters', 'Knits', 'IP', 'Denim and Woven Bottoms', 'Category Other', 'Accessories', '3P & Lic')]
+
+# Table with ELC
+View(spread_Vendor_ELC_table)
+
+# Table with Tier
+Vendor_Tier_Table <- Vendor_ELC_table %>% mutate("Tier" = NA)
+Vendor_Tier_Table <- Vendor_Tier_Table[c(1:6, 8, 7)]
+View(Vendor_Tier_Table)
+
+Join_table <- left_join(Delim_Table, Vendor_Tier_Table, by = c('MasterVendorID', 'Category', "PAR_VENDOR_ID", "VENDOR_ID"))
